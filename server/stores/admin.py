69ec684f4957006d38204553df1bea2d1ffc79e5
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2022-09-17 00:53:48 -0400

    add limit

--- server/stores/admin.py
+++ server/stores/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from stores.models import Store, AccessibilityCategory
 
 # Register your models here.
+admin.site.register(Store)
+admin.site.register(AccessibilityCategory)
--- server/stores/models.py
+++ server/stores/models.py
@@ -1,18 +1,34 @@
 from django.db import models
+from users.models import User
 
 
 class Store(models.Model):
-    name = models.TextField(max_length=40)
-    owner_firstname = models.TextField()
-    owner_lastname = models.TextField()
+    name = models.CharField(max_length=40)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner_fist_name = models.CharField(max_length=40)
+    owner_last_name = models.CharField(max_length=40)
     lat = models.IntegerField()
     long = models.IntegerField()
     video_tour = models.FileField(upload_to='videos', blank=True)
 
+    def __str__(self):
+        return self.name
+
+
+CAT_CHOICES = (
+    ("PARKING", "Parking"),
+    ("RAMP", "Ramp"),
+    ("ELEVATORS", "Elevators"),
+)
+
 
 class AccessibilityCategory(models.Model):
+    name = models.CharField(max_length=40, choices=CAT_CHOICES, default='PARKING')
     satisfied = models.BooleanField(default=False)
     image_evidence = models.ImageField(upload_to='images')
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
