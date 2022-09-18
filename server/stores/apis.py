74dbd5674074205ab9b357c7f5380649b80b1e74
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2022-09-17 21:16:29 -0400

    a

--- server/stores/apis.py
+++ server/stores/apis.py
@@ -1,13 +1,22 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+from django.views.decorators.csrf import csrf_exempt
 from stores.serializers import StoreSerializer, AccessibilityCategorySerializer
 from stores.models import Store, AccessibilityCategory
+
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+
+    def enforce_csrf(self, request):
+        return  # To not perform the csrf check previously happening
 
 
 class StoreViewSet(viewsets.ModelViewSet):
     serializer_class = StoreSerializer
     queryset = Store.objects.all()
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+
     # def perform_create(self, serializer):
     #     print("PRINT USER")
     #     print(self.request.user)
@@ -25,6 +34,7 @@ class StoreViewSet(viewsets.ModelViewSet):
 
 class AccessibilityCategoryViewSet(viewsets.ModelViewSet):
     serializer_class = AccessibilityCategorySerializer
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 
     def get_queryset(self):
         return AccessibilityCategory.objects.filter(store_id=self.kwargs.get('store_id'))
