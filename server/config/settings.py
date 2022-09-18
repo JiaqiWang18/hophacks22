da80bb1fad68af1a1cdab28f9c4367010f0132b8
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2022-09-17 20:25:47 -0400

    a

--- server/config/settings.py
+++ server/config/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env('.env')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('DJANGO_SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = env.str('DJANGO_SECRET_KEY') or os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
